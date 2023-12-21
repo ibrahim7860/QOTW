1adaf0e2043ac4af62435e0c604bc5d6d1230bf5
NayeemBelal <nayeem.belal.1@gmail.com>
2023-12-21 16:17:00 -0600

    moving to components folder

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { UserProfile } from "./components/UserProfile";
 import { FriendProfile } from "./components/FriendProfile";
+import { Login } from "./components/Login";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <UserProfile firstName="Nayeem" lastName="Belal" username="dababy1212" />
+      <Login />
     </View>
   );
 }
@@ -14,5 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#291400",
   },
 });
