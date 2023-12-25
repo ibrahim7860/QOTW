88a0ca60a0c7df0ba279c9d98464c14e86710be9
Ibrahim Ahmed <ibrahim.ahmed1564@gmail.com>
2023-12-24 20:34:18 -0600

    added context for friendrequests

--- components/FriendRequests.js
+++ components/FriendRequests.js
@@ -9,92 +9,19 @@ import {
 } from "react-native";
 import { FriendItem } from "./FriendItem";
 import { Button } from "./Button";
-import defaultProfilePic from "../assets/default.jpeg";
 import { SearchBar } from "./SearchBar";
 import { useFriends } from "./FriendsContext";
 
 export const FriendRequests = () => {
-  const { addFriend } = useFriends();
-  const [FriendRequests, setFriendRequests] = useState([
-    {
-      id: "1",
-      fullName: "Uzair Qureshi",
-      username: "fat_guy",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "2",
-      fullName: "Ibrahim Ahmed",
-      username: "yourdad",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "3",
-      fullName: "Nayeem Belal",
-      username: "dababy1212",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "4",
-      fullName: "John Doe",
-      username: "john_doe",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "5",
-      fullName: "Jane Doe",
-      username: "jane_doe",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "6",
-      fullName: "Big Man",
-      username: "big_man",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "7",
-      fullName: "Gangatron Rex",
-      username: "gang_rex",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "8",
-      fullName: "Saad Syed",
-      username: "saad_syed",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "9",
-      fullName: "Silly Sully",
-      username: "silly_sully",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-    {
-      id: "10",
-      fullName: "Zubi Goat",
-      username: "zubi_goat",
-      profilePicUri: defaultProfilePic,
-      isFriend: false,
-    },
-  ]);
+  const { friendRequests, addFriend, removeFriendRequest } = useFriends();
 
-  const [filteredFriends, setFilteredFriends] = useState(FriendRequests);
+  const [filteredFriends, setFilteredFriends] = useState(friendRequests);
 
   const handleSearch = (query) => {
     if (query.trim() === "") {
-      setFilteredFriends(FriendRequests);
+      setFilteredFriends(friendRequests);
     } else {
-      const filtered = FriendRequests.filter((friend) =>
+      const filtered = friendRequests.filter((friend) =>
         friend.fullName.toLowerCase().includes(query.toLowerCase())
       );
       setFilteredFriends(filtered);
@@ -102,24 +29,17 @@ export const FriendRequests = () => {
   };
 
   useEffect(() => {
-    setFilteredFriends(FriendRequests);
-  }, [FriendRequests]);
+    setFilteredFriends(friendRequests);
+  }, [friendRequests]);
 
   const acceptFriendRequest = (id) => {
-    const friendToAdd = FriendRequests.find((friend) => friend.id === id);
+    const friendToAdd = friendRequests.find((friend) => friend.id === id);
     friendToAdd.isFriend = true;
 
     if (friendToAdd) {
       addFriend(friendToAdd);
-
-      setFriendRequests((currentRequests) =>
-        currentRequests.filter((friend) => friend.id !== id)
-      );
+      removeFriendRequest(id);
     }
-  };
-
-  const removeFriendRequest = (id) => {
-    setFriendRequests(FriendRequests.filter((friend) => friend.id !== id));
   };
 
   return (
--- components/FriendsContext.js
+++ components/FriendsContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
+import defaultProfilePic from "../assets/default.jpeg";
 
 const FriendsContext = createContext();
 
@@ -7,6 +8,79 @@ export const useFriends = () => useContext(FriendsContext);
 export const FriendsProvider = ({ children }) => {
   const [friends, setFriends] = useState([
     // Your initial friends data
+  ]);
+
+  const [friendRequests, setFriendRequests] = useState([
+    {
+      id: "1",
+      fullName: "Uzair Qureshi",
+      username: "fat_guy",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "2",
+      fullName: "Ibrahim Ahmed",
+      username: "yourdad",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "3",
+      fullName: "Nayeem Belal",
+      username: "dababy1212",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "4",
+      fullName: "John Doe",
+      username: "john_doe",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "5",
+      fullName: "Jane Doe",
+      username: "jane_doe",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "6",
+      fullName: "Big Man",
+      username: "big_man",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "7",
+      fullName: "Gangatron Rex",
+      username: "gang_rex",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "8",
+      fullName: "Saad Syed",
+      username: "saad_syed",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "9",
+      fullName: "Silly Sully",
+      username: "silly_sully",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
+    {
+      id: "10",
+      fullName: "Zubi Goat",
+      username: "zubi_goat",
+      profilePicUri: defaultProfilePic,
+      isFriend: false,
+    },
   ]);
 
   const addFriend = (newFriend) => {
@@ -19,8 +93,27 @@ export const FriendsProvider = ({ children }) => {
     );
   };
 
+  const addFriendRequest = (newRequest) => {
+    setFriendRequests((currentRequests) => [...currentRequests, newRequest]);
+  };
+
+  const removeFriendRequest = (id) => {
+    setFriendRequests((currentRequests) =>
+      currentRequests.filter((request) => request.id !== id)
+    );
+  };
+
   return (
-    <FriendsContext.Provider value={{ friends, addFriend, removeFriend }}>
+    <FriendsContext.Provider
+      value={{
+        friends,
+        addFriend,
+        removeFriend,
+        friendRequests,
+        addFriendRequest,
+        removeFriendRequest,
+      }}
+    >
       {children}
     </FriendsContext.Provider>
   );
