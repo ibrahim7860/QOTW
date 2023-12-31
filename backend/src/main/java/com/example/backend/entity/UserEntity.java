182c319c11eae9546a1c56cf3d07a7f0b01dda56
NayeemBelal <nayeem.belal.1@gmail.com>
2023-12-30 23:55:52 -0600

    more changes

--- backend/src/main/java/com/example/backend/entity/UserEntity.java
+++ backend/src/main/java/com/example/backend/entity/UserEntity.java
@@ -2,10 +2,8 @@ package com.example.backend.entity;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 
 @Entity
