94119c5f148201194fa81cbd53447260ef6716eb
NayeemBelal <nayeem.belal.1@gmail.com>
2023-12-30 23:27:56 -0600

    added user table

---
+++ backend/src/main/java/com/example/backend/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.backend.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    private String userId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
